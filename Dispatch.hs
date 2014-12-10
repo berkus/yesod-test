63156b6ca304538750b225da104df603d428ed37
Berkus Decker <berkus@madfire.net>
2014-12-10 19:06:58 +0200

    Break out route handlers

--- Dispatch.hs
+++ Dispatch.hs
@@ -4,17 +4,9 @@
 
 module Dispatch where
 
-import Data.Default
 import Yesod
-import Yesod.Default.Util
 
 import Foundation
+import Handler.Home
 
 mkYesodDispatch "App" resourcesApp
-
-getHomeR :: Handler Html
-getHomeR = do
-    let filenames = ["readme.txt", "report.pdf", "music.mp3"] :: [String]
-    defaultLayout $ do
-        setTitle "File Processor"
-        $(widgetFileNoReload def "home")
