ed83571da412a864579aa6a62fa89a7d7dd4a3bd
Berkus Decker <berkus@madfire.net>
2014-12-10 19:01:16 +0200

    Split presentation and handlers

---
+++ Dispatch.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Dispatch where
+
+import Data.Default
+import Yesod
+import Yesod.Default.Util
+
+import Foundation
+
+mkYesodDispatch "App" resourcesApp
+
+getHomeR :: Handler Html
+getHomeR = do
+    let filenames = ["readme.txt", "report.pdf", "music.mp3"] :: [String]
+    defaultLayout $ do
+        setTitle "File Processor"
+        $(widgetFileNoReload def "home")
--- Main.hs
+++ Main.hs
@@ -1,24 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeFamilies #-}
-
 module Main where
 
-import Data.Default
 import Yesod
-import Yesod.Default.Util
 
-data App = App
-instance Yesod App
-
-mkYesod "App" $(parseRoutesFile "config/routes")
-
-getHomeR :: Handler Html
-getHomeR = defaultLayout $ do
-    let filenames = ["readme.txt", "report.pdf", "music.mp3"] :: [String]
-    setTitle "File Processor"
-    $(widgetFileNoReload def "home")
+import Dispatch ()
+import Foundation
 
 main :: IO ()
 main = warpEnv App
